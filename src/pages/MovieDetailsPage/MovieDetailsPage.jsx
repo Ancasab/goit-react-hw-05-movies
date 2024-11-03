cce8a93ebace6eab6010a3dc9bc2c8ba6d3b9b61
Anca_Sab <asabadeanu@gmail.com>
2024-11-03 22:27:59 +0200

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -58,7 +58,7 @@ const MovieDetailsPage = () => {
                 <img className={css.img} src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
                 <div className={css.text}>
                     <h2>{`${title} (${release_date.slice(0, 4)})`}</h2>
-                    <p>{`User score: ${vote_average}%`}</p>
+                    <p>{`User score: ${vote_average * 10}%`}</p>
                     <h3>Overview</h3>
                     <p>{overview}</p>
                     <h3>Genres</h3>
